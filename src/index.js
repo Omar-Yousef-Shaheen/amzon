64fd78b978cbc45d7a20a27b6024800fcabc639c
omar <Omaryousef6>
2024-08-19 18:01:30 +0300

    Update Path

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import GlobalState from "./Context/GlobalState";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router>
+    <Router basename="/amzon">
       <GlobalState>
         <App />
       </GlobalState>
